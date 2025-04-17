35cd462b61c4040f1d6b65782e652d81bd43397e
Rivza Adrian <rvzadrian@Rivzas-MacBook-Pro.local>
2025-04-17 15:53:35 +0700

    add cart function

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const user = require("./user/user.js");
-
+const cart = require('./cart/cart.js');
 const router = express.Router();
 
 router.post("/bni-e-commerce/register", user.registerUser);
 router.post("/bni-e-commerce/login", user.loginUser);
+router.post('/bni-e-commerce/cart/add/:id', cart.addToCart);
+router.get('/bni-e-commerce/cart/', cart.getProduct);
 
 module.exports = router;

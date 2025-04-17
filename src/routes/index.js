26052888db2c22607ad6b56c27e47bdf02f68502
unknown <aryarahmadian@gmail.com>
2025-04-17 16:49:09 +0700

    add product.js & edit index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const user = require("./user/user.js");
+const product = require("./product/product.js")
 
 const router = express.Router();
 
 router.post("/bni-e-commerce/register", user.registerUser);
 router.post("/bni-e-commerce/login", user.loginUser);
+
+router.get("/bni-e-commerce/products", product.getAllProduct);
+router.get("/bni-e-commerce/product/:id", product.getProductDetail);
 
 module.exports = router;
